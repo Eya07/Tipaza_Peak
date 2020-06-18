998e1226ad80ae29b285c65c065de27e34a686dd
Eya07 <eya.dhamna@esprit.tn>
2020-06-18 04:22:22 -0700

    final

---
+++ enigme/Quiz.eNIG/quiz.h
@@ -0,0 +1,17 @@
+/**
+* @file quiz.h
+* @brief Testing Program.
+* @eya dhamna
+* @version 0.1
+* @date juin 06, 2020
+*
+* Testing program enigme
+*
+*/
+#ifndef QUIZ_H_INCLUDED
+#define QUIZ_H_INCLUDED
+void reponse(SDL_Surface *ecran,int d);
+void solution(SDL_Surface *ecran,int d);
+void quiz(SDL_Surface *ecran,int d);
+void good(SDL_Surface *ecran);
+#endif // QUIZ_H_INCLUDED
